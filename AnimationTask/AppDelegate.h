925ed95eec58afbff402a3cdc55a9243d0c77d5d
Mikhail Yaskou <mikhailyaskow@gmail.com>
2017-08-17 10:42:38 +0300

    Initial Commit

---
+++ AnimationTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationTask
+//
+//  Created by Mikhail Yaskou on 17.08.17.
+//  Copyright © 2017 Mikhail Yaskou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
